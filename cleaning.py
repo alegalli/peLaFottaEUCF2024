35680f7a90567e2ddf6d363978186af39c3318f8
Ale <aaagalli@gmail.com>
2025-02-26 07:36:18 +0100

    cleaning progress points_player, init game_player

--- cleaning.py
+++ cleaning.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import numpy as np
 from pathlib import Path
 
 # Define input file pattern (adjust path if needed)
@@ -47,17 +48,20 @@ stat_recurrency = []
 # player stats 20 elems
 stat_short_names = ['S', 'D', 'HD', 'LT', 'T', 'P', 'Assist', 'Mete', 'Mete g att','Mete g dif','Mete g tot',"Possesses Played Offence","Possesses Played Defence","Possesses Played Tot", 'Mga att (min)', '(sec)', 'Mga dif (min)', '(sec).1', 'Mga tot (min)', '(sec).2']
 stat_names = ["super","block","help_defence","terrible_throw","throwaway","drop","assist","score","pts_played_o","pts_played_d","pts_played_tot","pss_played_o","pss_played_d","pss_played_tot","min_played_o","sec_played_o","min_played_d","sec_played_d","min_played_tot","sec_played_tot"]
-stat_recurrency = ["pt","pt","pt","pt","pt","pt","ps","ps","gm","gm","gm","pt","pt","pt","pt","pt","pt","pt","pt","pt"] # ps=ps+pt+gm, pt=pt+gm, gm=gm
+stat_recurrency = ["pt","pt","pt","pt","pt","pt","ps","ps","gm","gm","gm","pt","pt","pt","gm","gm","gm","gm","gm","gm"] # ps=ps+pt+gm, pt=pt+gm, gm=gm
 stat_dtype = ["int"]*len(stat_names)
 stat_owners = ["player"]*len(stat_names)
 stat_important = ['', '!', '!', '!', '!', '!', '', '', '', '', '!', '', '', '!', '', '', '', '', '', ''] # [""]*len(stat_names)
+# print(len(kpi_names))
+# print(len(stat_important))
+# print(len(stat_important))
 #team stats: 19 elems, Lineup.dtype = [], Break.dtype = bool
-t_stat_short_names = ["In Campo", "Blocks","Tovs","Break","A/D","","","","","","Schema A","Schema D","Storia neg","Storia pos","Assist","Meta","","","","","","","","","","","",""]
-t_stat_names = ["lineup","blocks","tovs","break","start_offensive_pt","offensive_ps","scored_us","scored_ps","score_us_ps","score_opp_ps","set_play_o","d_tactic","negative_hystory","positive_hystory","assistmen","scorer","tot_blocks","tot_tovs","ht_blocks","ht_tovs","tot_break_us","tot_break_opp","ht_break_us","ht_break_opp","final_score_us","final_score_opp","ht_score_us","ht_score_opp"]
-t_stat_recurrency = ["pt","pt","pt","pt","pt","ps","pt","ps","ps","ps","pt","pt","pt","pt","ps","ps","gm","gm","gm","gm","gm","gm","gm","gm","gm","gm","gm","gm"] # lineup is per point, we still don't manage perfectly the injury subs
-t_stat_dtype = ["[7]","int","int","bool","bool","bool","bool","bool","int","int","string","string","string","string","int","int","int","int","int","int","int","int","int","int","int","int","int","int"]
+t_stat_short_names = ["","In Campo","Blocks","Tovs","Break","","A/D","","","","","","Schema A","Schema D","Storia neg","Storia pos","Assist","Meta","","","","","","","","","","","",""]
+t_stat_names = ["special_line","lineup","blocks","tovs","break","clutch_point","start_offensive_pt","offensive_ps","scored_us","scored_ps","score_us_ps","score_opp_ps","set_play_o","d_tactic","negative_hystory","positive_hystory","assistmen","scorer","tot_blocks","tot_tovs","ht_blocks","ht_tovs","tot_break_us","tot_break_opp","ht_break_us","ht_break_opp","final_score_us","final_score_opp","ht_score_us","ht_score_opp"]
+t_stat_recurrency = ["pt","pt","pt","pt","pt","pt","pt","ps","pt","ps","ps","ps","pt","pt","pt","pt","ps","ps","gm","gm","gm","gm","gm","gm","gm","gm","gm","gm","gm","gm"] # lineup is per point, we still don't manage perfectly the injury subs
+t_stat_dtype = ["bool","[7]","int","int","bool","bool","bool","bool","bool","bool","int","int","string","string","string","string","int","int","int","int","int","int","int","int","int","int","int","int","int","int"]
 t_stat_owners = ["team"]*len(t_stat_names)
-t_stat_important = ['!', '', '', '!', '!', '!', '!', '!', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''] # [""]*len(t_stat_names)
+t_stat_important = ['', '!', '', '', '!', '!', '!', '!', '!', '!', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''] # [""]*len(t_stat_names)
 
 # print(len(t_stat_short_names))
 # print(len(t_stat_names))
@@ -261,26 +265,29 @@ def extract_table3(df, tot_row):
     if not numeric_start.any():
         raise ValueError("Could not find the numeric start for table 3.")
     start_index = numeric_start[numeric_start].index[0]
+
     # Header row is the row right before numeric start
     header_row = df.loc[start_index - 1].dropna().tolist()
     # print(header_row)
-    header_row = ["jersey_number","name"]+header_row
-    # print(header_row)
-    # Find the last column for table 3 — last occurrence of '(sec)' in the header row
-    last_col_index = next((i for i, col in enumerate(header_row) if 'Blocks linea D' in str(col)), len(header_row) - 1) - 1
-    # Find the last row — last numeric value in the first column
+    header_row = ["jersey_number","name"]+header_row[0:11]+["sec_played_o"]+[header_row[12]]+["sec_played_d"]+header_row[14:16]+["sec_played_tot"]
+    # print(header_row) #14 16 19
+
+    # last column
+    last_col_index = next((i for i, col in enumerate(header_row) if 'Blocks linea D' in str(col)), len(header_row) - 1)
+    # print(last_col_index)
+    # last row
     numeric_values = df.loc[start_index:, 0].apply(lambda x: str(x).strip().isdigit())
     if not numeric_values.any():
         raise ValueError("Could not find the last numeric row for table 3.")
     end_index = numeric_values[numeric_values].index[-1]
-    # Extract table 3
+
+    # Extract table 
     table3 = df.loc[start_index:end_index, :last_col_index].reset_index(drop=True)
-    # print(table3)
-    # print(header_row[:last_col_index + 1])
-    # print(table3.shape)
-    # print(len(header_row[:last_col_index + 1]))
     table3.columns = header_row[:last_col_index + 1]
     table3 = table3[table3['name'].notna()]
+
+
+    # return data_table3, columns_table3
     return table3
 
 
@@ -291,7 +298,6 @@ def extract_tables(filepath, last_point_id):
     in_campo_row = df[df.apply(lambda x: x.str.contains('In campo', na=False).any(), axis=1)].index[0]
     # in_campo_header = df.iloc[in_campo_row].dropna().tolist()
     in_campo_header = df.iloc[in_campo_row].tolist()
-    # in_campo_header.remove("TODO")
     in_campo_header[0] = "special_line"
 
     table1_start = in_campo_row + 1
@@ -306,105 +312,144 @@ def extract_tables(filepath, last_point_id):
     table1 = table1.drop(columns=table1.columns[2:10])  # Drop those 7 individual columns
     table1['point_id'] = range(last_point_id, last_point_id + len(table1))
     point_id = last_point_id + len(table1)
-    print(table1)
-    
-    
-    # print(len(in_campo_header))
-    # # print(table1.num_columns)
-    # print(len(in_campo_header))
-
     # print(table1)
-    # print(in_campo_header)
-
 
     # Table 2: Row starting with "Tot"
-    tot_row = df[df.iloc[:, 0].astype(str).str.contains('Tot', na=False)].index[0]
-    table2 = pd.DataFrame([df.iloc[tot_row, 10:14].tolist()], columns=["tot_blocks", "tot_tovs", "tot_breaks_us", "tot_breaks_opp"])
+    tot_A_row = df[df.iloc[:, 0].astype(str).str.contains('Tot', na=False)].index[0]
+    # TODO: trying to recover the data in a list instead of a DataFrame
+    data_table2 = [df.iloc[tot_A_row, 10:14].tolist()]
+    if (i==0):
+        col_table2 = ["tot_blocks", "tot_tovs", "tot_breaks_us", "tot_breaks_opp"]
+    table2 = pd.DataFrame([df.iloc[tot_A_row, 10:14].tolist()], columns=["tot_blocks", "tot_tovs", "tot_breaks_us", "tot_breaks_opp"])
     # print(table2)
 
-    # # Table 3: Starts with first numeric value after Table 2
-    # numeric_start = df[df.iloc[tot_row + 1:, 0].apply(lambda x: str(x).isdigit())].index[0]
-    # header_row = numeric_start - 1
-    # table3_end = numeric_start
-
-    # while table3_end < len(df) and pd.to_numeric(df.iloc[table3_end, 0], errors='coerce').notna():
-    #     table3_end += 1
-
-    # table3 = df.iloc[numeric_start:table3_end].reset_index(drop=True)
-    # print(table3)
-    # table3.columns = df.iloc[header_row].dropna().tolist()
-
-
-
     # # Table 3: Starts from next numeric row, ends at last numeric row
-    # numeric_start = df[df.iloc[tot_row + 1:, 0].apply(lambda x: str(x).strip().isdigit())].index[0]
-    # header_row = numeric_start - 1
-    # end_row_3 = df.loc[numeric_start:, 0].last_valid_index()
-
-    # # End of table 3 based on last "(sec)" column
-    # last_col_3 = df.loc[header_row].last_valid_index()
-    # table_3 = df.loc[numeric_start:end_row_3, :last_col_3].reset_index(drop=True)
-    # table_3.columns = df.loc[header_row, :last_col_3].tolist()
-    table3 = extract_table3(df, tot_row)
+    table3 = extract_table3(df, tot_A_row)
     # print(table3)
 
     # Table 4: Last row after "TOT", sharing headers with Table 3
     tot_final_row = df[df.iloc[:, 1].astype(str).str.contains('TOT', na=False)].index[0]
     table4_columns = table3.columns[2:]
-    print(len(df.iloc[tot_final_row, 2:].tolist()[:(len(table4_columns))]))
-    print(df.iloc[tot_final_row, 2:].tolist()[:(len(table4_columns))])
-    print(tot_final_row)
-    print(len(table4_columns))
     # print(df.iloc[tot_final_row, 2:].tolist())
     table4 = pd.DataFrame([df.iloc[tot_final_row, 2:].tolist()[:(len(table4_columns))]], columns=table4_columns)
-    print(table4)
+    # print(table4)
 
+    # return data_table1, column_table1,data_table2, column_table2,data_table3, column_table3,data_table4, column_table4, point_id
     return table1, table2, table3, table4, point_id 
 
 
 
 # Usage
-tables = []
+tables1 = []
+tables2 = []
+tables3 = []
+tables4 = []
 last_point_id = 0
 for (i, file) in enumerate(game_files):
     table1, table2, table3, table4, last_point_id = extract_tables(file, last_point_id)
     table1['game_id'] = i
-    table2['game_id'] = i
-    table3['game_id'] = i
+    table3['game_id'] = i # concat tables2 & tables4
     table4['game_id'] = i
-    tables.append(table1)
-    tables.append(table2)
-    tables.append(table3)
-    tables.append(table4)
+    tables1.append(table1)
+    tables2.append(table2)
+    tables3.append(table3)
+    tables4.append(table4)
 
-    # Display results
-    print("Table 1:")
-    print(table1.head())
-    print("\nTable 2:")
-    print(table2)
-    print("\nTable 3:")
-    print(table3.head())
-    print("\nTable 4:")
-    print(table4)
-    # tables = extract_tables("Stats La FOTTA EUCF 2024 - Tot.csv")
+# Display results
+print("Table 1:")
+# print(tables1)
+# print(tables1.head())
+print("\nTable 2:")
+print(tables2)
+print("\nTable 3:")
+print(tables3)
+# print(tables3.head())
+print("\nTable 4:")
+print(tables4)
 
-points_team = pd.DataFrame(columns=tables[0].columns)
-games_team = pd.DataFrame(columns=tables[1].columns+tables[1].columns)
-points_player = pd.DataFrame(columns=tables[2].columns)
-for i in range(0,4):
-    points_team = pd.concat([points_team,tables[4*i]], ignore_index=True)
+points_team = pd.DataFrame(columns=tables1[0].columns)
 
-    # games_team = pd.concat([games_team,tables[4*i+1]], ignore_index=True)
-    # games_team = pd.concat([games_team,tables[4*i+3]], ignore_index=True)
+print("games_team")
+# TODO: I know that I should save the data on lists and then only later work with dfs, to redo
+games_team = pd.concat([pd.concat(tables2, axis=0, ignore_index=True),pd.concat(tables4, axis=0, ignore_index=True)],axis=1)
+points_team = pd.concat(tables1, axis=0, ignore_index=True)
+points_player = pd.concat(tables3, axis=0, ignore_index=True)
+games_player = pd.concat(tables3, axis=0, ignore_index=True)
+# print(points_player)
+# print(points_player.columns)
+# games_df["game_id"] = range(1, len(games_df) + 1)
+# points_team_df = tables1.merge(games_df[["game_id"]], on="game_id", how="left")
+# points_player_df = tables3.merge(games_df[["game_id"]], on="game_id", how="left")
+# print(points_team_df)
+# print(points_player_df)
+# print(games_team)
+# print(points_team)
+# print(points_player)
+# print(games_team.columns[games_team.columns.duplicated()])
 
-    # points_player = pd.concat([points_player,tables[4*i+2]], ignore_index=True)
-    tables[4*i+1]
-    tables[4*i+2]
-    tables[4*i+3]
+#########
+# Rename columns
+#########
+# print(def_game_stats)
+# print(def_pt_stats)
+# print()
+pt_player_stats = input_stats_df[input_stats_df.recurrency == "pt"][input_stats_df.owners == "player"].names.reindex()
+ps_player_stats = input_stats_df[input_stats_df.recurrency == "ps"][input_stats_df.owners == "player"].names.reindex()
+gm_player_stats = input_stats_df[input_stats_df.recurrency == "gm"][input_stats_df.owners == "player"].names.reindex()
+pt_team_stats = input_stats_df[input_stats_df.recurrency == "pt"][input_stats_df.owners == "team"].names.reindex()
 
-print(points_team)
+mete_g_att = points_player.pop('Mete g att')
+mete_g_dif = points_player.pop('Mete g dif')
+mete_g_tot = points_player.pop('Mete g tot')
+mga_att = points_player.pop('Mga att (min)')
+mga_dif = points_player.pop('Mga dif (min)')
+mga_tot = points_player.pop('Mga tot (min)')
+sga_att = points_player.pop('sec_played_o')
+sga_dif = points_player.pop('sec_played_d')
+sga_tot = points_player.pop('sec_played_tot')
+points_player.insert(10, 'pss_played_o', np.nan)
+points_player.insert(11, 'pss_played_d', np.nan)
+points_player.insert(12, 'pss_played_tot', np.nan)
+print(points_player.columns)
+print(games_player.columns)
+print(pt_player_stats)
+print(ps_player_stats)
+print(gm_player_stats)
 
-# input_stats = {
+# print(f"points_player.columns[0:2]: {points_player.columns[0:2]}")
+# print(f"len(points_player.columns[0:2]): {len(points_player.columns[0:2])}")
+# print(f"pt_player_stats.to_list()[0:6]: {pt_player_stats.to_list()[0:6]}")
+# print(f"len(pt_player_stats.to_list()[0:6]): {len(pt_player_stats.to_list()[0:6])}")
+# print(f"ps_player_stats.to_list(): {ps_player_stats.to_list()}")
+# print(f"len(ps_player_stats.to_list()): {len(ps_player_stats.to_list())}")
+# print(f"points_player.columns[10:]: {points_player.columns[10:]}")
+# print(f"len(points_player.columns[10:]): {len(points_player.columns[10:])}")
+points_player.columns = ( #TODO: to edit
+    list(points_player.columns[0:2]) + # jersey_number, name
+    list(pt_player_stats.to_list()[0:6]) + #TODO: change base stats to game
+    list(ps_player_stats.to_list()) + # assist, score
+    list(points_player.columns[10:]) # pt_o,d,tot:bool, mga_o,d,tot, sga_o,d,tot ps_o,d,tot, game_id
+)
+# TODO: easy compile games_player
+# games_player.columns = (
+#     list(points_player.columns[0:2]) + # jersey_number, name
+#     list(pt_player_stats.to_list()[0:6]) + #TODO: change base stats to game
+#     list(ps_player_stats.to_list()) + # assist, score
+#     list(points_player.columns[10:]) # pss_o,d,tot, game_id
+# )
+# points_player.columns = points_player.columns[0:2]+pt_player_stats.to_list()[0:6]+ps_player_stats.to_list()+points_player.columns[10:] # TODO: to calc possessions_played and move points_played into game stats
+# print(points_player)
+
+in_campo = points_team.pop('In Campo')
+points_team.insert(1, 'In Campo', in_campo)
+points_team.insert(5, 'clutch_pt', np.nan) #'') # TODO: calc clutch_pt & scored_us
+points_team.insert(7, 'scored_us', np.nan)
+print(points_team.columns)
+print(pt_team_stats)
+points_team.columns = pt_team_stats.to_list()+["assist","score","point_id","game_id"]
+
+
+# print(points_team)# input_stats = {
 #     'short_names': stat_short_names+t_stat_short_names,
 #     'names': stat_names+t_stat_names,
 #     '!': stat_important+t_stat_important,
