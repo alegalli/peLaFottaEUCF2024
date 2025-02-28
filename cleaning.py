c48516a1eab3e86ebdb7872bf4e7c23045a4a778
Ale <aaagalli@gmail.com>
2025-02-28 02:11:52 +0100

    refactoring points-team

--- cleaning.py
+++ cleaning.py
@@ -45,23 +45,34 @@ stat_short_names = []
 stat_owners = []
 stat_recurrency = []
 # Taken Manually from pd.read_csv("C:\\Users\\Alessandro\\Documents\\ale\\peLaFottaEUCF2024\\Stats La FOTTA EUCF 2024\\Stats La FOTTA EUCF 2024 - BADSKID.csv")
-# player stats 20 elems
-stat_short_names = ['S', 'D', 'HD', 'LT', 'T', 'P', 'Assist', 'Mete', 'Mete g att','Mete g dif','Mete g tot',"Possesses Played Offence","Possesses Played Defence","Possesses Played Tot", 'Mga att (min)', '(sec)', 'Mga dif (min)', '(sec).1', 'Mga tot (min)', '(sec).2']
-stat_names = ["super","block","help_defence","terrible_throw","throwaway","drop","assist","score","pts_played_o","pts_played_d","pts_played_tot","pss_played_o","pss_played_d","pss_played_tot","min_played_o","sec_played_o","min_played_d","sec_played_d","min_played_tot","sec_played_tot"]
-stat_recurrency = ["pt","pt","pt","pt","pt","pt","ps","ps","gm","gm","gm","pt","pt","pt","gm","gm","gm","gm","gm","gm"] # ps=ps+pt+gm, pt=pt+gm, gm=gm
-stat_dtype = ["int"]*len(stat_names)
+# player stats 24 elems
+stat_short_names = ['S', 'D', 'HD', 'LT', 'T', 'P', 'Assist', 'Mete', 'Possesses Played Offence','Possesses Played Defence','Possesses Played Tot', 'Mete g att','Mete g dif','Mete g tot', 'Mga att (min)', '(sec)', 'Mga dif (min)', '(sec).1', 'Mga tot (min)', '(sec).2']
+#not going to add info not strictly related to players AND points eg: clutch_point, special_line, break, scored_us, start_offensive_pt (shared in team_points, go find them there) # TODO: I think I can remove the time stats in player_points and put them in team_points, other than team_game
+stat_names = ["super","block","help_defence","terrible_throw","throwaway","drop","assist","score","pss_played_o","pss_played_d","pss_played_tot","pts_played_o","pts_played_d","pts_played_tot","min_played_o","sec_played_o","min_played_d","sec_played_d","min_played_tot","sec_played_tot"] 
+# stat_names_gm = ["super","block","help_defence","terrible_throw","throwaway","drop","assist","score","pss_played_o","pss_played_d","pss_played_tot","pts_played_o","pts_played_d","pts_played_tot","min_played_o","sec_played_o","min_played_d","sec_played_d","min_played_tot","sec_played_tot"]
+stat_recurrency = ["pt","pt","pt","pt","pt","pt","ps","ps","pt","pt","pt","pt","pt","pt","pt","pt","pt","pt","pt","pt"] # ps=ps+pt+gm, pt=pt+gm, gm=gm
+stat_dtype_pt = ["int","int","int","int","int","int","bool","bool","int","int","int","bool","bool","bool","int","int","int","int","int","int"] # 
+stat_dtype_gm = ["int"]*len(stat_names) # 
 stat_owners = ["player"]*len(stat_names)
 stat_important = ['', '!', '!', '!', '!', '!', '', '', '', '', '!', '', '', '!', '', '', '', '', '', ''] # [""]*len(stat_names)
 # print(len(kpi_names))
 # print(len(stat_important))
 # print(len(stat_important))
 #team stats: 19 elems, Lineup.dtype = [], Break.dtype = bool
-t_stat_short_names = ["","In Campo","Blocks","Tovs","Break","","A/D","","","","","","Schema A","Schema D","Storia neg","Storia pos","Assist","Meta","","","","","","","","","","","",""]
-t_stat_names = ["special_line","lineup","blocks","tovs","break","clutch_point","start_offensive_pt","offensive_ps","scored_us","scored_ps","score_us_ps","score_opp_ps","set_play_o","d_tactic","negative_hystory","positive_hystory","assistmen","scorer","tot_blocks","tot_tovs","ht_blocks","ht_tovs","tot_break_us","tot_break_opp","ht_break_us","ht_break_opp","final_score_us","final_score_opp","ht_score_us","ht_score_opp"]
-t_stat_recurrency = ["pt","pt","pt","pt","pt","pt","pt","ps","pt","ps","ps","ps","pt","pt","pt","pt","ps","ps","gm","gm","gm","gm","gm","gm","gm","gm","gm","gm","gm","gm"] # lineup is per point, we still don't manage perfectly the injury subs
-t_stat_dtype = ["bool","[7]","int","int","bool","bool","bool","bool","bool","bool","int","int","string","string","string","string","int","int","int","int","int","int","int","int","int","int","int","int","int","int"]
+t_stat_short_names = ["","","","In Campo","Blocks","Tovs","Break","","A/D","","","","","","Schema A","Schema D","Storia neg","Storia pos","Assist","Meta","","","","","","","","","","","",""]
+t_stat_names = ["min_pt","sec_pt","special_line","lineup","blocks","tovs","break","clutch_point","start_offensive_pt","offensive_ps","scored_us","scored_ps","score_us_ps","score_opp_ps","set_play_o","d_tactic","negative_hystory","positive_hystory","assistmen","scorer","tot_blocks","tot_tovs","ht_blocks","ht_tovs","tot_break_us","tot_break_opp","ht_break_us","ht_break_opp","final_score_us","final_score_opp","ht_score_us","ht_score_opp"]
+# t_stat_names = ["min_pt","sec_pt","special_line","lineup","blocks","tovs","break","clutch_point","start_offensive_pt","offensive_ps","scored_us","scored_ps","score_us_ps","score_opp_ps","set_play_o","d_tactic","negative_hystory","positive_hystory","assistmen","scorer","tot_blocks","tot_tovs","ht_blocks","ht_tovs","tot_break_us","tot_break_opp","ht_break_us","ht_break_opp","final_score_us","final_score_opp","ht_score_us","ht_score_opp"]
+t_stat_recurrency = ["pt","pt","pt","pt","pt","pt","pt","pt","pt","ps","pt","ps","ps","ps","pt","pt","pt","pt","ps","ps","gm","gm","gm","gm","gm","gm","gm","gm","gm","gm","gm","gm"] # lineup is per point, we still don't manage perfectly the injury subs
+t_stat_dtype = ["int","int","bool","[7]","int","int","bool","bool","bool","bool","bool","bool","int","int","string","string","string","string","int","int","int","int","int","int","int","int","int","int","int","int","int","int"]
 t_stat_owners = ["team"]*len(t_stat_names)
-t_stat_important = ['', '!', '', '', '!', '!', '!', '!', '!', '!', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''] # [""]*len(t_stat_names)
+t_stat_important = ['','','', '!', '', '', '!', '!', '!', '!', '!', '!', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''] # [""]*len(t_stat_names)
+
+# print(len(stat_short_names))
+# print(len(stat_names))
+# print(len(stat_recurrency))
+# print(len(stat_dtype_pt))
+# print(len(stat_owners))
+# print(len(stat_important))
 
 # print(len(t_stat_short_names))
 # print(len(t_stat_names))
@@ -75,7 +86,8 @@ input_stats = {
     '!': stat_important+t_stat_important,
     'recurrency': stat_recurrency+t_stat_recurrency,
     'owners': stat_owners+t_stat_owners,
-    'dtype': stat_dtype+t_stat_dtype
+    'dtype_ps': stat_dtype_pt+t_stat_dtype,
+    'dtype_gm': stat_dtype_gm+t_stat_dtype
 }
 input_stats_df = pd.DataFrame(input_stats)
 # print(input_stats_df.to_string())
@@ -256,9 +268,9 @@ ps = calc_ps_team()
 
 
 
-##################
-## Big Cleaning ##
-##################
+####################
+## Big Collecting ##
+####################
 def extract_table3(df, tot_row):
     # Find the starting row of table 3 — first numeric value after 'Tot' row
     numeric_start = df.iloc[tot_row + 1:, 0].apply(lambda x: str(x).strip().isdigit())
@@ -307,6 +319,7 @@ def extract_tables(filepath, last_point_id):
 
     table1 = df.iloc[table1_start:table1_end].reset_index(drop=True)
 
+    # TODO: in not collecting the min_pt && sec_pt for team_points. # Time not used for this project
     table1.columns = in_campo_header
     table1['In Campo'] = table1.iloc[:, 3:10].values.tolist()  # Group first 7 columns
     table1 = table1.drop(columns=table1.columns[2:10])  # Drop those 7 individual columns
@@ -355,17 +368,17 @@ for (i, file) in enumerate(game_files):
     tables3.append(table3)
     tables4.append(table4)
 
-# Display results
-print("Table 1:")
-# print(tables1)
-# print(tables1.head())
-print("\nTable 2:")
-print(tables2)
-print("\nTable 3:")
-print(tables3)
-# print(tables3.head())
-print("\nTable 4:")
-print(tables4)
+# # Display results
+# print("Table 1:")
+# # print(tables1)
+# # print(tables1.head())
+# print("\nTable 2:")
+# print(tables2)
+# print("\nTable 3:")
+# print(tables3)
+# # print(tables3.head())
+# print("\nTable 4:")
+# print(tables4)
 
 points_team = pd.DataFrame(columns=tables1[0].columns)
 
@@ -373,8 +386,9 @@ print("games_team")
 # TODO: I know that I should save the data on lists and then only later work with dfs, to redo
 games_team = pd.concat([pd.concat(tables2, axis=0, ignore_index=True),pd.concat(tables4, axis=0, ignore_index=True)],axis=1)
 points_team = pd.concat(tables1, axis=0, ignore_index=True)
-points_player = pd.concat(tables3, axis=0, ignore_index=True)
+# points_player = pd.concat(tables3, axis=0, ignore_index=True)
 games_player = pd.concat(tables3, axis=0, ignore_index=True)
+
 # print(points_player)
 # print(points_player.columns)
 # games_df["game_id"] = range(1, len(games_df) + 1)
@@ -398,23 +412,90 @@ ps_player_stats = input_stats_df[input_stats_df.recurrency == "ps"][input_stats_
 gm_player_stats = input_stats_df[input_stats_df.recurrency == "gm"][input_stats_df.owners == "player"].names.reindex()
 pt_team_stats = input_stats_df[input_stats_df.recurrency == "pt"][input_stats_df.owners == "team"].names.reindex()
 
-mete_g_att = points_player.pop('Mete g att')
-mete_g_dif = points_player.pop('Mete g dif')
-mete_g_tot = points_player.pop('Mete g tot')
-mga_att = points_player.pop('Mga att (min)')
-mga_dif = points_player.pop('Mga dif (min)')
-mga_tot = points_player.pop('Mga tot (min)')
-sga_att = points_player.pop('sec_played_o')
-sga_dif = points_player.pop('sec_played_d')
-sga_tot = points_player.pop('sec_played_tot')
-points_player.insert(10, 'pss_played_o', np.nan)
-points_player.insert(11, 'pss_played_d', np.nan)
-points_player.insert(12, 'pss_played_tot', np.nan)
-print(points_player.columns)
-print(games_player.columns)
-print(pt_player_stats)
-print(ps_player_stats)
-print(gm_player_stats)
+print(input_stats_df)
+
+
+#########
+# Points-Player
+#########
+# Empty Points-Player columns TODO: recalc table from team_pt stats
+# NOTE: do not initialize with NaN values, TODO: add possession_id
+# points_player = points_player[list(points_player.columns[0:2])]
+# print(pt_player_stats)
+# points_player = pd.DataFrame({})
+points_player_columns = ( #
+    list(players_df.columns[0:2]) + # jersey_number, name
+    list(input_stats_df[input_stats_df.owners == "player"].names.to_list()) + #all player stats
+    ["point_id","game_id"]
+)
+print(len(points_player_columns))
+print(points_player_columns)
+# TODO: recalc from Points-Team
+
+
+#########
+# Points-Team
+#########
+points_team.insert(0, 'min_pt', np.nan)
+points_team.insert(1, 'sec_pt', np.nan)
+in_campo = points_team.pop('In Campo')
+points_team.insert(3, 'In Campo', in_campo)
+points_team.insert(5, 'clutch_pt', np.nan) #'') # TODO: calc clutch_pt & scored_us
+points_team.insert(7, 'scored_us', np.nan)
+print(pt_team_stats)
+
+points_team.columns = pt_team_stats.to_list()+["assist","score","point_id","game_id"]
+
+# Calculation Coposed Fields: clutch_point, scored_us
+# Clutch Point Definition:
+# 3pt left to the game point
+# 2pt left to the ht_point
+# AND
+# 2pt or less difference (difficult to score more than 2 consecutive breaks)
+# Index(['min_pt', 'sec_pt', 'special_line', 'lineup', 'blocks', 'tovs', 'break', 'clutch_point', 'start_offensive_pt', 'scored_us', 'set_play_o', 'd_tactic', 'negative_hystory', 'positive_hystory', 'assist', 'score', 'point_id', 'game_id'],
+# NEED: games_team.final_score_us, games_team.final_score_opp, game_team.ht_score_us, game_team.ht_score_opp
+# "scored_us","scored_ps","score_us_ps","score_opp_ps",
+# "tot_blocks","tot_tovs","ht_blocks","ht_tovs","tot_break_us","tot_break_opp","ht_break_us","ht_break_opp","final_score_us","final_score_opp","ht_score_us","ht_score_opp"
+print(games_team)
+print(games_team)
+
+
+
+
+# print(points_team.columns)
+# print("points_team")
+print(points_team.columns)
+# print(games_player)
+# print(games_team)
+
+
+
+# # for (i, col) in enumerate(pt_player_stats):
+#     points_player.insert(2+i, col, np.nan)
+
+# print(points_player)# points_player
+
+# print(points_player[list(points_player.columns[0:2]) + list(points_player.columns[10:])])
+# # TODO: A different method to select 
+# mete_g_att = points_player.pop('Mete g att')
+# mete_g_dif = points_player.pop('Mete g dif')
+# mete_g_tot = points_player.pop('Mete g tot')
+# mga_att = points_player.pop('Mga att (min)')
+# mga_dif = points_player.pop('Mga dif (min)')
+# mga_tot = points_player.pop('Mga tot (min)')
+# sga_att = points_player.pop('sec_played_o')
+# sga_dif = points_player.pop('sec_played_d')
+# sga_tot = points_player.pop('sec_played_tot')
+# points_player.insert(10, 'pss_played_o', np.nan)
+# points_player.insert(11, 'pss_played_d', np.nan)
+# points_player.insert(12, 'pss_played_tot', np.nan)
+# print(points_player.columns)
+# print(games_player.columns)
+# print(pt_player_stats)
+# print(ps_player_stats)
+# print(gm_player_stats)
+
+
 
 # print(f"points_player.columns[0:2]: {points_player.columns[0:2]}")
 # print(f"len(points_player.columns[0:2]): {len(points_player.columns[0:2])}")
@@ -424,29 +505,23 @@ print(gm_player_stats)
 # print(f"len(ps_player_stats.to_list()): {len(ps_player_stats.to_list())}")
 # print(f"points_player.columns[10:]: {points_player.columns[10:]}")
 # print(f"len(points_player.columns[10:]): {len(points_player.columns[10:])}")
-points_player.columns = ( #TODO: to edit
-    list(points_player.columns[0:2]) + # jersey_number, name
-    list(pt_player_stats.to_list()[0:6]) + #TODO: change base stats to game
-    list(ps_player_stats.to_list()) + # assist, score
-    list(points_player.columns[10:]) # pt_o,d,tot:bool, mga_o,d,tot, sga_o,d,tot ps_o,d,tot, game_id
-)
-# TODO: easy compile games_player
-# games_player.columns = (
+# points_player.columns = ( #TODO: to edit
 #     list(points_player.columns[0:2]) + # jersey_number, name
 #     list(pt_player_stats.to_list()[0:6]) + #TODO: change base stats to game
 #     list(ps_player_stats.to_list()) + # assist, score
-#     list(points_player.columns[10:]) # pss_o,d,tot, game_id
+#     list(points_player.columns[10:]) # pt_o,d,tot:bool, mga_o,d,tot, sga_o,d,tot ps_o,d,tot, game_id
 # )
+
+# TODO: easy compile games_player
+games_player.columns = (
+    list(games_player.columns[0:2]) + # jersey_number, name
+    list(pt_player_stats.to_list()[0:6]) + #TODO: change base stats to game
+    list(ps_player_stats.to_list()) + # assist, score
+    list(games_player.columns[10:]) # pss_o,d,tot, game_id
+)
 # points_player.columns = points_player.columns[0:2]+pt_player_stats.to_list()[0:6]+ps_player_stats.to_list()+points_player.columns[10:] # TODO: to calc possessions_played and move points_played into game stats
 # print(points_player)
 
-in_campo = points_team.pop('In Campo')
-points_team.insert(1, 'In Campo', in_campo)
-points_team.insert(5, 'clutch_pt', np.nan) #'') # TODO: calc clutch_pt & scored_us
-points_team.insert(7, 'scored_us', np.nan)
-print(points_team.columns)
-print(pt_team_stats)
-points_team.columns = pt_team_stats.to_list()+["assist","score","point_id","game_id"]
 
 
 # print(points_team)# input_stats = {
